85592680cc7fa2f2da5a780ec738e8719390ccc7
Jarno Lindholm <lindholm.jarno@gmail.com>
2020-12-30 20:12:31 +0200

    (day-6) Solution to part two

--- day6/day6.py
+++ day6/day6.py
@@ -17,5 +17,29 @@ def solve():
     print(result)
 
 
+def solve_part_two():
+    data = get_input()
+    answers = {}
+    result = 0
+    counter = 0
+    for line in data:
+        if line == '':
+            old_result = result
+            for val in answers.values():
+                if val == counter:
+                    result += 1
+            answers = {}
+            counter = 0
+        else:
+            counter += 1
+        for c in line:
+            if c in answers:
+                answers[c] += 1
+            else:
+                answers[c] = 1
+    print(result)
+
+
 if __name__ == '__main__':
     solve()
+    solve_part_two()
